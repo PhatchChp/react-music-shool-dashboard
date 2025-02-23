a328547e94ba7e6b5ca2e08e4c053fc9258fd6c8
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-02-23 15:30:48 +0700

    create AuthContext & useAuth

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import Dashboard from "./components/page/dashboard/Dashboard";
 import Notfound from "./components/page/notfound/Notfound";
 import Login from "./components/page/auth/Login";
 import Logout from "./components/page/auth/Logout";
+import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Dashboard />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="logout" element={<Logout />} />
-                    <Route path="*" element={<Notfound />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Dashboard />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="logout" element={<Logout />} />
+                        <Route path="*" element={<Notfound />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     );
 }
 
---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { AuthContext } from "./AuthContext";
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<string | null>(null);
+    const login = (username: string) => setUser(username);
+    const logout = () => setUser(null);
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
