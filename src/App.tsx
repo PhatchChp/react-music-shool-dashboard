e5c3fe7f6340a4c3f4db67abc4d9b4f71086b9eb
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-02-24 14:17:44 +0700

    create: AppRoute & update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Layout from "./laout/Layout";
-import Dashboard from "./components/page/dashboard/Dashboard";
-import Notfound from "./components/page/notfound/Notfound";
-import Login from "./components/page/auth/Login";
-import Logout from "./components/page/auth/Logout";
 import { AuthProvider } from "./context/AuthProvider";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
     return (
         <AuthProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Dashboard />} />
-                        <Route path="dashboard" element={<Dashboard />} />
-                        <Route path="login" element={<Login />} />
-                        <Route path="logout" element={<Logout />} />
-                        <Route path="*" element={<Notfound />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
+            <AppRoutes />
         </AuthProvider>
     );
 }
