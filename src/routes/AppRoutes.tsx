a49b0ec44413de7366a7165ae35fbade457e4120
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-02-24 21:36:41 +0700

    remove: logout route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,7 +3,6 @@ import Login from "../components/page/auth/Login";
 import PrivateRoute from "./PrivateRoute";
 import Layout from "../laout/Layout";
 import Dashboard from "../components/page/dashboard/Dashboard";
-import Logout from "../components/page/auth/Logout";
 import Notfound from "../components/page/notfound/Notfound";
 
 const AppRoutes = () => {
@@ -15,7 +14,6 @@ const AppRoutes = () => {
                     <Route element={<Layout />}>
                         <Route index element={<Dashboard />} />
                         <Route path="dashboard" element={<Dashboard />} />
-                        <Route path="logout" element={<Logout />} />
                     </Route>
                 </Route>
                 <Route path="*" element={<Notfound />} />
