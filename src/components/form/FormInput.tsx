0427e5bde26a9b06bfcb8336c8c40e30fb9e614e
PhatchChp <Phatcharaphon.chp@gmail.com>
2025-03-10 23:02:26 +0700

    update: formInput add validate react-hook-form

--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,36 +1,55 @@
-interface FormInputProps {
+import { FieldValues, Path, UseFormRegister } from "react-hook-form";
+
+interface FormInputProps<T extends FieldValues> {
     label: string;
-    name: string;
+    name: Path<T>;
     id?: string;
-    type: string;
+    type?: string;
     placeholder?: string;
     defaultValue?: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    required?: boolean;
+    disabled?: boolean;
+    autoComplete?: React.HTMLInputAutoCompleteAttribute;
+    register?: UseFormRegister<T>;
+    error?: string | React.ReactNode;
     className?: string;
 }
 
-const FormInput = ({
+const FormInput = <T extends FieldValues>({
     label,
     name,
-    id = name,
-    type,
+    id,
+    type = "text",
     placeholder,
     defaultValue,
-    onChange,
+    required = false,
+    disabled = false,
+    autoComplete = "off",
+    register,
+    error,
     className = "",
-}: FormInputProps) => {
+}: FormInputProps<T>) => {
     return (
-        <div className={className}>
-            <label htmlFor={id}>{label}</label>
+        <div className={`flex flex-col gap-1 ${className}`}>
+            <label htmlFor={id || name} className="font-medium text-gray-700">
+                {label} {required && <span className="text-red-500">*</span>}
+            </label>
             <input
-                name={name}
+                {...(register ? register(name) : {})} // รองรับการใช้งานนอก react-hook-form
+                id={id || name}
                 type={type}
                 placeholder={placeholder}
                 defaultValue={defaultValue}
-                className="p-2 w-full border-1 border-gray-200 rounded-md shadow-md mt-1"
-                onChange={onChange}
+                disabled={disabled}
+                autoComplete={autoComplete}
+                className={`p-2 w-full border rounded-md shadow-sm transition focus:ring focus:ring-blue-300 ${
+                    error ? "border-red-500" : "border-gray-300"
+                }`}
+                required={required}
             />
+            {error && <p className="text-red-500 text-sm">{error}</p>}
         </div>
     );
 };
+
 export default FormInput;
